b07098eac1072c9a82964a1a60a6cfd5cb57374b
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-18 16:04:02 -0500

    Completed Second Experiment

--- vue-d3/src/App.vue
+++ vue-d3/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <main>
+    <h1>Testing D3 in Vue</h1>
+    <h4>First Chart Test</h4>
     <Ex1 />
+    <h4>Second Chart Test</h4>
+    <Ex2 />
   </main>
 </template>
 
 <script>
 import Ex1 from "@/components/Ex1.vue";
+import Ex2 from "@/components/Ex2.vue";
 
 export default {
   name: 'App',
   components: {
-    Ex1
+    Ex1,
+    Ex2,
   }
 }
 </script>
@@ -29,5 +35,9 @@ main{
   display:flex;
   flex-direction: column;
   justify-content: space-around;
+}
+
+h4{
+  text-decoration: underline;
 }
 </style>
