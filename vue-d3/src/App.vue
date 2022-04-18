4b306b40d3ae1c5b82575117735940f3adfdd67b
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-18 13:09:09 -0500

    Initial Setup

--- vue-d3/src/App.vue
+++ vue-d3/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <main>
-    <h1>Hello World</h1>
+    <Ex1 />
   </main>
 </template>
 
 <script>
+import Ex1 from "@/components/Ex1.vue";
 
 export default {
   name: 'App',
+  components: {
+    Ex1
+  }
 }
 </script>
 
---
+++ vue-d3/src/components/Ex1.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+    name: "Ex1",
+}
+</script>
+
+<style>
+
+</style>
