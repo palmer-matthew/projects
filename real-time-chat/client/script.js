36e59f92c5b317fe57b116ebda0f17b9727955e5
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-17 20:21:38 -0500

    Completed Setup

---
+++ real-time-chat/client/script.js
@@ -0,0 +1,26 @@
+const joinRoomButton = document.getElementById("room-button");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const message = messageInput.value;
+    const room = roomInput.value;
+
+    if(message === "") return ;
+    displayMessage(message);
+
+    messageInput.value = "";
+});
+
+joinRoomButton.addEventListener("click", () => {
+    const room = roomInput.value;
+});
+
+function displayMessage(message){
+    const div = document.createElement("div");
+    div.textContent = message;
+    document.getElementById("message-content").append(div);
+}
