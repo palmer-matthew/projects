1c2d1c0e5b3da001fed26d927c46a4a1b84f105f
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-19 12:09:21 -0500

    Started Working on Ex4

--- vue-d3/src/App.vue
+++ vue-d3/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <main>
     <h1>Testing D3 in Vue</h1>
-    <h4>First Chart Test</h4>
+    <h4>First Experiment</h4>
     <Ex1 />
-    <h4>Second Chart Test</h4>
+    <h4>Second Experiment</h4>
     <Ex2 />
-    <h4>Third Chart Test</h4>
+    <h4>Third Experiment</h4>
     <Ex3 />
+    <h4>Fourth Experiment</h4>
+    <Ex4 />
   </main>
 </template>
 
@@ -14,6 +16,7 @@
 import Ex1 from "@/components/Ex1.vue";
 import Ex2 from "@/components/Ex2.vue";
 import Ex3 from "@/components/Ex3.vue";
+import Ex4 from "@/components/Ex4.vue";
 
 export default {
   name: 'App',
@@ -21,6 +24,7 @@ export default {
     Ex1,
     Ex2,
     Ex3,
+    Ex4,
   }
 }
 </script>
---
+++ vue-d3/src/components/Ex4.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <!-- <button> Add Node </button> -->
+        <div id="chart4"></div>
+    </section>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+    name: "Ex4",    
+    data(){
+        return {
+            width: 960,
+            height: 500,
+        };
+    },
+    methods:{
+        init(){
+            // const fill = d3.scale.category20();
+
+            const svg = d3.select("#chart4")
+                        .append("svg")
+                        .attr("width", this.width)
+                        .attr("height", this.height);
+        },
+    },
+    mounted(){
+        this.init();
+    }
+}
+</script>
