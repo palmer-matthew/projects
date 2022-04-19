15b3d7ec9fb09810829ac44f03e79361fca8de0d
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-18 19:09:09 -0500

    Experiment 3 completed somewhat

--- vue-d3/README.md
+++ vue-d3/README.md
@@ -10,3 +10,8 @@ npm install
 1) Familiarity with D3 library
 2) Build Model Component Draft [Capstone]
 3) Learn more about SVGs
+
+## Tutorials Followed:
+1. [Data visualization with Vue and D3](https://blog.logrocket.com/data-visualization-with-vue-and-d3/)
+2. [Data Visualization with Vue and D3](https://alligator.io/vuejs/visualization-vue-d3/)
+3. [D3.js and Vue.js](https://dev.to/andre347/d3js-and-vuejs--30c8)
--- vue-d3/src/components/Ex3.vue
+++ vue-d3/src/components/Ex3.vue
@@ -4,18 +4,61 @@
 
 <script>
 import * as d3 from "d3";
+import tweets from "@/assets/tweets.json";
 
 export default {
     name: "Ex3",    
     data(){
         return {
-            width: 700,
-            height: 500,
+            width: 600,
+            height: 600,
         };
     },
     methods:{
         init(){
             const svg = d3.select("#chart3").append("svg").attr("width", this.width).attr("height", this.height);
+
+            const colourScale = d3.scaleOrdinal().range(["#5EAFC6","#FE9922","#93C464","#75739F"]);
+
+            const nestedTweets = d3.group(tweets.tweets, d => d.user);//.key(d => d.key).entries(tweets);
+
+            const packed = {id : "ALL", values: nestedTweets};
+
+            const packedData = d3.hierarchy(packed, d => d.values)
+            .sum(d => d.retweets ? d.retweets.length + d.favorites.length + 1 : 1);
+
+            // console.log(packedData)
+
+            const packChart = d3.pack().size([600, 600]).padding(10);
+
+            const output = packChart(packedData).descendants();
+
+            const pChart = output.map((d, i) => {
+                const fill = colourScale(d.depth);
+
+                return {
+                    id: i + 1,
+                    r: d.r,
+                    x: d.x,
+                    y: d.y,
+                    fill,
+                    stroke: "grey"
+                };
+            });
+
+            // console.log(pChart)
+
+            const g = svg.append("g").style("transform", "translate(50,50)");
+
+            g.selectAll("circle")
+                .data(pChart)
+                .enter()
+                .append("circle")
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+                .attr("r", d => d.r)
+                .attr("fill", d => d.fill)                
+                .attr("stroke", d => d.stroke);              
         },
     },
     mounted(){
