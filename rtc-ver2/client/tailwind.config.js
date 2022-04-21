a080cccd675ebb0146759a168e6e408fe4f24639
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-20 19:01:10 -0500

    Update Tialwind Config I

--- rtc-ver2/client/tailwind.config.js
+++ rtc-ver2/client/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "red-hat": ['"Red Hat Text"', 'sans-serif']
+      },
+      color:{
+        primary: '#01101B',
+        grey: '#2D3944',
+        accent: '#27B281'
+      }
+    },
   },
   plugins: [],
 }
